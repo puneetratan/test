6afe5980e73a825e5d1a1b03645a518f845996e6
puneetratan <puneetsiet@gmail.com>
2013-07-05 10:21:25 +0530

    COMMIT WITH MORE DETIALS

--- myapp.js
+++ myapp.js
@@ -1,10 +1,30 @@
 function DomLoaded() {
 	document.addEventListener('deviceready',phonegapLoaded,false);
+	
 }
 
 function phonegapLoaded() {
+	
+	var element = document.getElementById('deviceInfo');
+	element.innerHTML = 'Device name '+ device.name + '</br />'+ 
+						'Phonegap version '	+ device.phonegap  	+ '</br />'+
+						'Platform '	+ device.platform  	+ '</br />'+
+						'Device UUID '	+ device.uuid  	+ '</br />'+
+						'OS version '	+ device.version  	+ '</br />';
+						
+	networkConnection();							
+						
 }
 
+function beep() { navigator.notification.beep(4);}
+
+function vibrate() { navigator.notification.vibrate(2000);}
+
+function networkConnection() {
+	var networkState = navigator.network.connection.type;
+	alert('Network states ' + networkState);
+}
+/*
 function alert() {
 	navigator.notification.alert(
 		'Hello world',
@@ -12,10 +32,27 @@ function alert() {
 		'Archana',
 		'Done'
 	);
+} */
+
+function demo1() {
+	navigator.notification.confirm(
+		'Hello world',
+		onCofirm,
+		'Archana',
+		'Done1,Done2'
+	);
 }
+
+
 
 function dismissed() {
 	$(function() {
-		$("body").append("<h1>Dismissed</h1>");
+		$("body").html("<h1>Dismissed</h1>");
+	});
+}
+
+function onCofirm(button) {
+	$(function() {
+		$("#result").html('puneet');
 	});
 }
