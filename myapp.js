ce0f83e8b449795b28a75ff99d022fd67fa0409b
puneetratan <puneetsiet@gmail.com>
2013-07-05 06:55:54 +0530

    third commit

--- myapp.js
+++ myapp.js
@@ -1,10 +1,11 @@
 function DomLoaded() {
+	alert("Hi Archana, this app dedicated to u")
 	document.addEventListener('deviceready',phonegapLoaded,false);
 }
 
 function phonegapLoaded() {
 	$(function() {
-		$('body').append('<h1>Device is Ready</h1>')
+		$("body").append("<h1>Device is Ready</h1>");
 	});
 	
 }
